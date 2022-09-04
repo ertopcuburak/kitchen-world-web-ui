056fdc32502a3cf5cf15a494a969504f36567613
Burak Ertopcu <burak.ertopcu@yandex.com>
2022-09-05 00:47:57 +0300

    ios changes

--- src/app/utils/environment.ts
+++ src/app/utils/environment.ts
@@ -1,6 +1,6 @@
 export class Environment {
     //public static apiUrl = "http://localhost:8080";
-    public static apiUrl = "https://ac06-95-70-175-230.eu.ngrok.io";
+    public static apiUrl = "https://9bfd-95-70-175-230.eu.ngrok.io";
     public static migrosSearchUrl = "https://www.migros.com.tr/arama?q=";
     public static trendyolSearchUrl = "https://www.trendyol.com/sr?q=";
 }
